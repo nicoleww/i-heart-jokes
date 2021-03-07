6103c8ded76f124ff97261f8e6a9fa0746e87e19
Nicole WItherall <nicwitherall@gmail.com>
2021-03-07 16:00:16 -0500

    add animation to burger menu

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const nav = document.querySelector('.nav-links');
 // event listeners
 burger.addEventListener('click', function() {
     nav.classList.toggle('nav-active');
+    burger.classList.toggle('exit');
 });
 
 
