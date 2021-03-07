6309991e758b51caa4fdaede94a5612c89e402de
nicoleww <nicwitherall@gmail.com>
2021-03-07 15:43:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// cached elemoents
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+// event listeners
+burger.addEventListener('click', function() {
+    nav.classList.toggle('nav-active');
+});
+
+
